06a5be315cf3c4ab2e3a6a149da54744332caeb5
GabrielSilper <gabrielsilper.main@gmail.com>
2023-05-25 22:06:07 -0400

    R01 - Testes feito e requisito ok.

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -2,8 +2,8 @@ import { Request, Response } from 'express';
 import productService from '../services/product.service';
 
 const create = async (req: Request, res: Response) => {
-  const { type, status, message } = await productService.create(req.body);
-  if (type) return res.status(status).json({ message });
+  const { status, message } = await productService.create(req.body);
+  // if (type) return res.status(status).json({ message });
   return res.status(status).json(message);
 };
 
